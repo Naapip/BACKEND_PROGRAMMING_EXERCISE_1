101675a191576cf7ced348139786d4d83132e1d3
Naapip <161700519+Naapip@users.noreply.github.com>
2024-03-07 16:53:15 +0700

    535230204 Naufal Afif

---
+++ soal_1.js
@@ -0,0 +1,14 @@
+function BilPrima(num) {
+    for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+for(let i = 2; i <= 1000; i++) {
+    if(BilPrima(i)) {
+        console.log(i);
+    }
+}
